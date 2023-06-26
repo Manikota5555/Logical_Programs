81fdf0314173a7943a79b5740a11f072e0761274
Manikota5555 <127322129+Manikota5555@users.noreply.github.com>
2023-06-26 23:55:57 +0530

    Add files via upload

---
+++ PriPerfectNum.java
@@ -0,0 +1,16 @@
+public class PriPerfectNum{
+  public static void main(String args[]){
+    int sum=0;
+    for(int i=0;i<100;i++){
+      sum=0;
+      for(int j=1;j<i;j++){
+        if(i%j==0){
+          sum=sum+j;
+        }
+      }
+      if(sum==i && sum!=0){
+        System.out.print(i+" ");
+      }
+    }
+  }
+}
