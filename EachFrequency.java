b85be6243220b0fe10ce43f6f40c4deb4d51c971
Manikota5555 <127322129+Manikota5555@users.noreply.github.com>
2023-06-27 09:51:47 +0530

    Add files via upload

---
+++ EachFrequency.java
@@ -0,0 +1,20 @@
+public class EachFrequency{
+  public static void main(String args[]){
+    String str="hi hello welcome to bitlabs";
+    char ch[]=new char[str.length()];
+    ch=str.toCharArray();
+    int count=1;
+    for(int i=0;i<ch.length;i++){
+      for(int j=i+1;j<ch.length;j++){
+        if(ch[i]==ch[j]){
+          count++;
+          ch[j]=' ';
+        }
+      }
+      if(ch[i]!=' '){
+        System.out.println(ch[i]+" : "+count);
+      }
+      count=1;
+    }
+  }
+}
